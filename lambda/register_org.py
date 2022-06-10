2f37d4f2f78e4cc6d55cd01debcf11f168b2a86a
Justin Harris <justin.harris@crowdstrike.com>
2022-06-10 19:10:10 +0100

    S3 bucket contents

--- lambda/register_org.py
+++ lambda/register_org.py
@@ -63,34 +63,7 @@ def get_master_id():
 def lambda_handler(event, context):
     logger.info('Got event {}'.format(event))
     logger.info('Context {}'.format(context))
-    #  Exampe response to post request
-    # resources = [
-    #     {
-    #         "CreatedAt": "2021-07-01T19:47:32.787Z",
-    #         "DeletedAt": "2021-07-01T19:47:32.787Z",
-    #         "ID": 0,
-    #         "UpdatedAt": "2021-07-01T19:47:32.787Z",
-    #         "account_id": "string",
-    #         "aws_cloudtrail_bucket_name": "string",
-    #         "aws_cloudtrail_region": "string",
-    #         "aws_permissions_status": [
-    #             {
-    #                 "name": "string",
-    #                 "status": "string"
-    #             }
-    #         ],
-    #         "cid": "string",
-    #         "cloudformation_url": "string",
-    #         "eventbus_name": "string",
-    #         "external_id": "string",
-    #         "iam_role_arn": "string",
-    #         "intermediate_role_arn": "string",
-    #         "is_master": true,
-    #         "organization_id": "string",
-    #         "status": "string"
-    #     }
-    #
-    # ]
+
     CFT = boto3.client('cloudformation')
     OrgId, AccountId = get_master_id()
 
@@ -107,7 +80,7 @@ def lambda_handler(event, context):
         # Format post message
         try:
             # Execute the command by calling the named function
-            response_data = {}
+
             logger.info('Event = {}'.format(event))
             response = falcon.create_aws_account(account_id=AccountId, organization_id=OrgId, cloudtrail_region=aws_region)
             if response['status_code'] == 400:
@@ -128,22 +101,24 @@ def lambda_handler(event, context):
                     "aws_eventbus_arn": response_data.get('aws_eventbus_arn', ''),
                     "account_type": response_data.get('account_type', '')
                 }
-                cfnresponse_send(event, context, SUCCESS, response_d,"CustomResourcePhysicalID")
+                cfnresponse_send(event, context, SUCCESS, response_d, "CustomResourcePhysicalID")
             else:
                 response_d = {}
-                cfnresponse_send(event, context, FAILED, response_d,"CustomResourcePhysicalID")
+                cfnresponse_send(event, context, FAILED, response_d, "CustomResourcePhysicalID")
         except Exception as err:  # noqa: E722
             # We can't communicate with the endpoint
             logger.info('Registration Failed {}'.format(err))
             cfnresponse_send(event, context, FAILED, err, "CustomResourcePhysicalID")
 
     elif event['RequestType'] in ['Update']:
+        response_data = {}
         logger.info('Event = ' + event['RequestType'])
-        cfnresponse_send(event, context, SUCCESS, "CustomResourcePhysicalID")
+        cfnresponse_send(event, context, SUCCESS, response_data, "CustomResourcePhysicalID")
 
     elif event['RequestType'] in ['Delete']:
         logger.info('Event = ' + event['RequestType'])
         response = falcon.delete_aws_account(organization_ids=OrgId)
+        response_data = {}
         CFT.delete_stack(
             StackName="CrowdStrike-CSPM-Integration")
-        cfnresponse_send(event, context, 'SUCCESS', response, "CustomResourcePhysicalID")
+        cfnresponse_send(event, context, 'SUCCESS', response_data, "CustomResourcePhysicalID")
