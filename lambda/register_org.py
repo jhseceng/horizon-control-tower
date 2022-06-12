9124d7d12ce1a8fde85506f38678f890cb760c5d
Justin Harris <justin.harris@crowdstrike.com>
2022-06-12 20:12:53 +0100

    Added commercial option to registration to support Gov

--- lambda/register_org.py
+++ lambda/register_org.py
@@ -63,7 +63,34 @@ def get_master_id():
 def lambda_handler(event, context):
     logger.info('Got event {}'.format(event))
     logger.info('Context {}'.format(context))
-
+    #  Exampe response to post request
+    # resources = [
+    #     {
+    #         "CreatedAt": "2021-07-01T19:47:32.787Z",
+    #         "DeletedAt": "2021-07-01T19:47:32.787Z",
+    #         "ID": 0,
+    #         "UpdatedAt": "2021-07-01T19:47:32.787Z",
+    #         "account_id": "string",
+    #         "aws_cloudtrail_bucket_name": "string",
+    #         "aws_cloudtrail_region": "string",
+    #         "aws_permissions_status": [
+    #             {
+    #                 "name": "string",
+    #                 "status": "string"
+    #             }
+    #         ],
+    #         "cid": "string",
+    #         "cloudformation_url": "string",
+    #         "eventbus_name": "string",
+    #         "external_id": "string",
+    #         "iam_role_arn": "string",
+    #         "intermediate_role_arn": "string",
+    #         "is_master": true,
+    #         "organization_id": "string",
+    #         "status": "string"
+    #     }
+    #
+    # ]
     CFT = boto3.client('cloudformation')
     OrgId, AccountId = get_master_id()
 
@@ -80,9 +107,12 @@ def lambda_handler(event, context):
         # Format post message
         try:
             # Execute the command by calling the named function
-
+            response_data = {}
             logger.info('Event = {}'.format(event))
-            response = falcon.create_aws_account(account_id=AccountId, organization_id=OrgId, cloudtrail_region=aws_region)
+            response = falcon.create_aws_account(account_id=AccountId,
+                                                 organization_id=OrgId,
+                                                 cloudtrail_region=aws_region,
+                                                 parameters={"account_type": "commercial"})
             if response['status_code'] == 400:
                 #
                 # We have an error
@@ -101,24 +131,31 @@ def lambda_handler(event, context):
                     "aws_eventbus_arn": response_data.get('aws_eventbus_arn', ''),
                     "account_type": response_data.get('account_type', '')
                 }
-                cfnresponse_send(event, context, SUCCESS, response_d, "CustomResourcePhysicalID")
+                cfnresponse_send(event, context, SUCCESS, response_d,"CustomResourcePhysicalID")
             else:
-                response_d = {}
-                cfnresponse_send(event, context, FAILED, response_d, "CustomResourcePhysicalID")
+                response_d = response['body']
+                cfnresponse_send(event, context, FAILED, response_d,"CustomResourcePhysicalID")
         except Exception as err:  # noqa: E722
             # We can't communicate with the endpoint
             logger.info('Registration Failed {}'.format(err))
             cfnresponse_send(event, context, FAILED, err, "CustomResourcePhysicalID")
 
     elif event['RequestType'] in ['Update']:
-        response_data = {}
+        response_d = {}
         logger.info('Event = ' + event['RequestType'])
-        cfnresponse_send(event, context, SUCCESS, response_data, "CustomResourcePhysicalID")
+        cfnresponse_send(event, context, SUCCESS, response_d, "CustomResourcePhysicalID")
 
     elif event['RequestType'] in ['Delete']:
         logger.info('Event = ' + event['RequestType'])
         response = falcon.delete_aws_account(organization_ids=OrgId)
-        response_data = {}
+        CFT.delete_stack(
+            StackName="CrowdStrike-CSPM-Integration-EB")
+        CFT.delete_stack(
+            StackName="CrowdStrike-CSPM-Root-EB")
         CFT.delete_stack(
             StackName="CrowdStrike-CSPM-Integration")
-        cfnresponse_send(event, context, 'SUCCESS', response_data, "CustomResourcePhysicalID")
+
+        cfnresponse_send(event, context, 'SUCCESS', response['body'], "CustomResourcePhysicalID")
+
+
+
